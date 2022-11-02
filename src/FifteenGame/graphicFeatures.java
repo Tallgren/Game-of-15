9d68aaa2157efeebb5974fb88f00f041c3ac5cf7
lemoniapd <lemonia.pd@gmail.com>
2022-11-02 09:01:50 +0100

    trying to get most recent version, commit clean slate

--- src/FifteenGame/graphicFeatures.java
+++ src/FifteenGame/graphicFeatures.java
@@ -8,45 +8,4 @@ import java.util.ArrayList;
 
 public class graphicFeatures extends JFrame implements ActionListener {
 
-    logicFeatures lf = new logicFeatures();
-    JPanel headPanel = new JPanel();
-    JPanel gamePanel = buttonPanel(lf.buttonList());
-    JButton newGame = new JButton("Nytt Spel");
-    JButton demo = new JButton("Demo");
-    graphicFeatures() {
-        setLayout(new BorderLayout());
-        add(headPanel, BorderLayout.NORTH);
-        add(gamePanel, BorderLayout.SOUTH);
-
-        headPanel.add(newGame);
-        headPanel.add(demo);
-        newGame.addActionListener(this);
-
-        pack();
-        setLocationRelativeTo(null);
-        setVisible(true);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-    }
-
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == newGame) {
-            ArrayList<JButton> buttonList = lf.buttonList();
-            lf.scrambledButtonList(buttonList);
-            for (JButton element : buttonList) {
-                JButton b = (JButton) gamePanel.getComponent(buttonList.indexOf(element));
-                b.setText(element.getText());
-            }
-        }
-    }
-
-    public JPanel buttonPanel(ArrayList<JButton> listToBeAdded){
-        JPanel buttonPanel = new JPanel();
-        buttonPanel.setLayout(new GridLayout(4, 4));
-        for (JButton element: listToBeAdded) {
-            buttonPanel.add(element);
-        }
-        buttonPanel.add(new JButton(""));
-        return buttonPanel;
-    }
 }
--- src/FifteenGame/logicFeatures.java
+++ src/FifteenGame/logicFeatures.java
@@ -6,36 +6,5 @@ import java.util.Collections;
 
 public class logicFeatures extends JFrame {
 
-    public ArrayList<JButton> buttonList() {
-        ArrayList<JButton> buttonList = new ArrayList<>();
-        for (int i = 1; i < 16; i++) {
-            JButton b = new JButton();
-            b.setText(String.valueOf(i)); //may need adjustments
-            buttonList.add(b);
-        }
-        return buttonList;
-    }
 
-    public void demoList(ArrayList<JButton> buttonList) {
-        buttonList.get(15).setText("");
-        buttonList.get(16).setText("15");
-    }
-
-    public void scrambledButtonList(ArrayList<JButton> buttonList) {
-        Collections.shuffle(buttonList);
-    }
-
-    public boolean isWin(ArrayList<JButton> orderOfButtons) {
-        int controller = 1;
-        for (JButton element : orderOfButtons) {
-            if (controller == Integer.parseInt(element.getText())) {
-                controller++;
-            }
-        }
-        if (controller == 15) {
-            return true;
-        } else {
-            return false;
-        }
-    }
 }
